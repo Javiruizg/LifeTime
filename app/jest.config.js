72e7b1cb8eb49ab6f82f6148b8c99b00f18a1b78
Javiruizg <2210jrg@gmail.com>
2026-05-16 00:37:24 +0200

    Added sonarcloud workflow and a few mock tests

---
+++ app/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'jest-expo',
+  testMatch: ['**/*.test.(ts|tsx)'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!**/.expo/**',
+    '!**/ios/**',
+    '!**/android/**',
+    '!**/*.test.{ts,tsx}',
+    '!**/index.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))',
+  ],
+  setupFiles: ['./jest.setup.js'],
+};
---
+++ server/src/__tests__/fail.test.ts
@@ -0,0 +1,5 @@
+describe('Forced failure smoke test', () => {
+  it('should fail (remove this file after verifying CI)', () => {
+    expect(true).toBe(false);
+  });
+});
